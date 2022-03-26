f09575ce649237d00a63d00b674f42e3d6d57af8
Ann-Sofie Jørgensen <annsofiej95@hotmail.com>
2022-03-26 12:38:04 +0100

    User Menu Tilføjet

--- UML 2 - Pizza/MenuCatalog.cs
+++ UML 2 - Pizza/MenuCatalog.cs
@@ -10,16 +10,33 @@ namespace UML_2_PizzaStore
 
         public List<Pizza> pizzaList = new List<Pizza>();
      
-        public void Start(Pizza pizza)
+        public void PizzaList()
         {
             Pizza pizza1 = new Pizza(1, "Hawaii", 85, "Ham, Pineapple, Cheese");
             Pizza pizza2 = new Pizza(2, "Veggie", 80, "Bellpepper, Olives, Mushrooms");
             Pizza pizza3 = new Pizza(3, "House Special", 90, "Kebab, Chili, Cheese");
+            Pizza pizza4 = new Pizza(4, "Capricosa", 60, "Mushrooms, Ham, Cheese");
             
             pizzaList.Add(pizza1);
             pizzaList.Add(pizza2);
             pizzaList.Add(pizza3);
+            pizzaList.Add(pizza4);
         }
+
+        public void UserMenu()
+        {
+            Console.WriteLine("Welcome To The Menu!");
+            Console.WriteLine();
+            Console.WriteLine("Please Select An Option!");
+            Console.WriteLine();
+            Console.WriteLine("1. Create A Pizza!");
+            Console.WriteLine("2. Show Full Menu!");
+            Console.WriteLine("3. Search For A Pizza By Number!");
+            Console.WriteLine("4. Search For A Pizza By Name!");
+            Console.WriteLine("5. Remove Pizza From Menu!");
+            Console.WriteLine("To Close Program Press 'x'");
+        }
+
 
         public void ShowFullMenu()
         {
@@ -29,7 +46,32 @@ namespace UML_2_PizzaStore
             }
         }
 
-        public void SearchForPizza(string searchPhrase)
+        public void CreatePizza()
+        {
+            string nameOfPizza;
+            double priceOfPizza;
+            string toppings;
+            int numberOfPizza;
+
+            
+            Console.WriteLine($"CREATE PIZZA");
+            Console.WriteLine($"____________");
+            Console.WriteLine($"Name Your Pizza:");
+            nameOfPizza = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Set Price For Pizza::");
+            priceOfPizza = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Add Your Toppings. Please seperate toppings with ',':");
+            toppings = Console.ReadLine();
+            Console.WriteLine($"You Have Now Created Your Pizza! Your Pizza Has Been Assigned Number {pizzaList.Count+1}");
+            numberOfPizza = pizzaList.Count + 1;
+            Pizza pizza = new Pizza(numberOfPizza, nameOfPizza, priceOfPizza, toppings);
+            pizzaList.Add(pizza);
+             
+        }
+
+       /* public void SearchForPizza(string searchPhrase)
         {
             var matchingPizzas = pizzaList.Where(p => p.NameOfPizza.Contains(searchPhrase)).ToList();
             foreach (Pizza pizza in matchingPizzas)
@@ -44,11 +86,11 @@ namespace UML_2_PizzaStore
             foreach (Pizza pizza in matchingPizzas)
             {
                 Console.WriteLine($"{pizza.NameOfPizza} - ({pizza.Toppings}) - {pizza.PriceOfPizza} DKK");
-            }
+            } */
         }
     
     }
-}
+
 
     
 
--- UML 2 - Pizza/Program.cs
+++ UML 2 - Pizza/Program.cs
@@ -6,55 +6,7 @@ namespace UML_2_PizzaStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("What Do You Want To Do?");
-            Console.WriteLine("1 Create New Pizza");
-            Console.WriteLine("2 Show Menu");
-            Console.WriteLine("3 Search For Pizza By Name");
-            Console.WriteLine("4 Remove Pizza From Menu");
-            Console.WriteLine("Press 'X' To Exit Program.");
-
-            var userInput = Console.ReadLine();
-
-            var menuCatalog = new MenuCatalog();
-
-            while (true)
-            {
-
-                switch (userInput)
-                {
-                    case "1":
-                        Console.WriteLine("Pizza Number:");
-                        var numberOfPizza = Console.ReadLine();
-                        Console.WriteLine("Pizza Name:");
-                        var nameOfPizza = Console.ReadLine();
-                        Console.WriteLine("Pizza Toppings. Please use ',' Between Each Topping!:");
-                        var toppings = Console.ReadLine();
-                        Console.WriteLine("Pizza Price:");
-                        var priceOfPizza = Console.ReadLine();
-
-                        Pizza p = new Pizza(Convert.ToInt32(numberOfPizza), nameOfPizza, Convert.ToDouble(priceOfPizza), toppings);
-                        pizzaList.Add(p);
-
-                        break;
-                    case "2":
-                        menuCatalog.ShowFullMenu();
-                        break;
-
-                    case "3":
-                        break;
-                    case "x":
-                        return;
-                    default:
-                        Console.WriteLine("Select Valid Option");
-                        break;
-
-                }
-                Console.WriteLine("Select New Option");
-                userInput = Console.ReadLine();
-            }
-
-
+            Store.RunApp();
         }
     }
 }
